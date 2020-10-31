4f5a3ac4f6a2a809a2c1fe2fe1e9b772d9b8cda4
Maycon <mayconjf@gmail.com>
2020-10-31 09:44:09 -0300

    Animação de tamanho e transparencia

--- lib/main.dart
+++ lib/main.dart
@@ -36,24 +36,27 @@ class _LogoAppState extends State<LogoApp> with SingleTickerProviderStateMixin {
       duration: Duration(seconds: 1),
     );
 
-    _animation = Tween<double>(begin: 0, end: 300).animate(_animationController)
-      ..addListener(() {
-        print('Listener - toda vez que a animação ocorre');
-      })
-      ..addStatusListener((status) {
-        if (status == AnimationStatus.completed) {
-          _animationController.reverse();
-        } else if (status == AnimationStatus.dismissed) {
-          _animationController.forward();
-        }
-      });
+    _animation =
+        CurvedAnimation(parent: _animationController, curve: Curves.elasticOut)
+          ..addListener(() {
+            print(
+                'Listener - toda vez que a animação ocorre, 1x por frame renderizado');
+          })
+          ..addStatusListener((status) {
+            if (status == AnimationStatus.completed) {
+              _animationController.reverse();
+            } else if (status == AnimationStatus.dismissed) {
+              _animationController.forward();
+            }
+          });
 
     _animationController.forward();
   }
 
   @override
   Widget build(BuildContext context) {
-    return AnimatedLogo(_animation);
+    return GrowAndOpacityTransition(
+        animation: _animation, childAnimation: LogoWidget());
   }
 
   @override
@@ -63,18 +66,57 @@ class _LogoAppState extends State<LogoApp> with SingleTickerProviderStateMixin {
   }
 }
 
-class AnimatedLogo extends AnimatedWidget {
-  AnimatedLogo(Animation<double> animation) : super(listenable: animation);
+// class AnimatedLogo extends AnimatedWidget {
+//   AnimatedLogo(Animation<double> animation) : super(listenable: animation);
+
+//   @override
+//   Widget build(BuildContext context) {
+//     Animation<double> animation = listenable;
+//     return Center(
+//       child: Container(
+//         height: animation.value,
+//         width: animation.value,
+//         child: FlutterLogo(),
+//       ),
+//     );
+//   }
+// }
+
+class LogoWidget extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(child: FlutterLogo());
+  }
+}
+
+class GrowAndOpacityTransition extends StatelessWidget {
+  final Animation<double> animation;
+  final Widget childAnimation;
+
+  final Tween<double> sizeTween = Tween(begin: 0, end: 300);
+  final Tween<double> opacityTween = Tween(begin: 0.1, end: 1);
+
+  GrowAndOpacityTransition(
+      {@required this.animation, @required this.childAnimation});
 
   @override
   Widget build(BuildContext context) {
-    Animation<double> animation = listenable;
     return Center(
-      child: Container(
-        height: animation.value,
-        width: animation.value,
-        child: FlutterLogo(),
-      ),
+      child: AnimatedBuilder(
+          child: childAnimation,
+          animation: animation,
+          builder: (context, child) {
+            return Opacity(
+              opacity: opacityTween.evaluate(animation).clamp(0, 1.0),
+              child: Container(
+                height:
+                    sizeTween.evaluate(animation).clamp(0.0, double.infinity),
+                width:
+                    sizeTween.evaluate(animation).clamp(0.0, double.infinity),
+                child: child,
+              ),
+            );
+          }),
     );
   }
 }
